7062e0161a541fa61ef6f399bf51d3888139f093
Alex Tran <alextran1357@gmail.com>
2023-10-04 13:07:13 -0700

    Reimplement PPO network

--- PPO/basic-ppo.py
+++ PPO/basic-ppo.py
@@ -1,61 +1,20 @@
-# Initialize policy parameters theta
-# Initialize value function parameters phi
-# Initialize optimization algorithm hyperparameters (e.g., learning rate, epsilon_clip)
-
-# for iteration = 1, 2, ..., N do
-#     # Collect trajectories using the current policy
-#     trajectories = collect_trajectories(policy = pi_theta)
-    
-#     # Calculate advantages and returns for each timestep in the trajectories
-#     advantages, returns = calculate_advantages_and_returns(trajectories, value_function = V_phi)
-    
-#     for optimization_step = 1, 2, ..., K do
-#         for trajectory in trajectories do
-#             for t = 1, 2, ..., T do
-#                 # Calculate the probability ratio
-#                 rho_t = pi_theta(trajectory.actions[t] | trajectory.states[t]) / pi_theta_old(trajectory.actions[t] | trajectory.states[t])
-                
-#                 # Calculate the clipped surrogate objective
-#                 L_clip = min(rho_t * advantages[t], clip(rho_t, 1 - epsilon_clip, 1 + epsilon_clip) * advantages[t])
-                
-#                 # Optional: Add a value function loss term and an entropy regularization term
-#                 # L = L_clip - c1 * value_function_loss + c2 * entropy_bonus
-                
-#                 # Calculate gradients of the objective w.r.t. policy parameters
-#                 gradients = compute_gradients(L_clip, w.r.t. = theta)
-                
-#                 # Update policy parameters
-#                 theta = optimize_policy(theta, learning_rate, gradients)
-                
-#                 # Optional: Update the value function parameters
-#                 # phi = optimize_value_function(returns, V_phi, states, learning_rate_vf)
-                
-#                 # Ensure numerical stability
-#                 theta, phi = enforce_numerical_stability(theta, phi)
-#             end for
-#         end for
-#     end for
-# end for
-
-# STILL WIP ---------------------------------------------------------------------------------
-
-import tensorflow as tf
-import gymnasium as gym
-import tensorflow_probability as tfp
 import numpy as np
+import tensorflow as tf
+import tensorflow_probability as tfp
+import gymnasium as gym
 
-class PolicyNetwork(tf.keras.Model):
-    def __init__(self, obs_space):
+class ActorModel(tf.keras.Model):
+    def __init__(self, action_size):
         super().__init__()
         self.d1 = tf.keras.layers.Dense(32, activation='relu')
-        self.out = tf.keras.layers.Dense(obs_space, activation='softmax')
+        self.out = tf.keras.layers.Dense(action_size,activation='softmax')
     
     def call(self, state):
         x = self.d1(state)
         x = self.out(x)
         return x
     
-class ValueNetwork(tf.keras.Model):
+class CriticModel(tf.keras.Model):
     def __init__(self):
         super().__init__()
         self.d1 = tf.keras.layers.Dense(32, activation='relu')
@@ -65,89 +24,116 @@ class ValueNetwork(tf.keras.Model):
         x = self.d1(state)
         x = self.out(x)
         return x
-    
-class Agent():
-    def __init__(self, obs_space, learning_rate=0.01, discount_factor=0.9, epsilon_clip=0.3, lambda_=0.95):
+
+class PPO:
+    def __init__(self, action_dim, lr=0.0002, gamma=0.9, epsilon=0.2, lambda_=0.95, c1=1.0, c2=0.01):
+        self.action_dim=action_dim
+        self.gamma = gamma
+        self.epsilon = epsilon
         self.lambda_ = lambda_
-        self.gamma = discount_factor
-        self.epsilon_clip = epsilon_clip
-        self.value_network = ValueNetwork()
-        self.policy_network = PolicyNetwork(obs_space)
-        self.value_opt = tf.keras.optimizers.Adam(learning_rate=learning_rate)
-        self.policy_opt = tf.keras.optimizers.Adam(learning_rate=learning_rate)
-        
-    def select_action(self, state):
-        probs = self.policy_network(state)
-        dist = tfp.distributions.Categorical(probs=probs)
+        self.c1 = c1   
+        self.c2 = c2
+
+        self.actor = ActorModel(action_dim)
+        self.critic = CriticModel()
+        self.actor_optimizer = tf.keras.optimizers.Adam(lr)
+        self.critic_optimizer = tf.keras.optimizers.Adam(lr)
+
+    def get_action(self, state):
+        softmax_prob = self.actor(np.array([state]))
+        dist = tfp.distributions.Categorical(probs=softmax_prob)
         action = dist.sample()
-        return int(action.numpy()[0]), probs[0]
+        action_prob = dist.prob(action)
+        return int(action.numpy()[0]), action_prob
     
-    def compute_advantages(self, states, rewards, dones):
-        advantages = np.zeros_like(rewards, dtype=np.float32)
-        returns = np.zeros_like(rewards, dtype=np.float32)
-        running_advantage = 0
-        values = [self.value_network(state) for state in tf.expand_dims(states, 0)]
-        running_return = values[-1]
-        
+    def compute_gae(self, rewards, values, next_values, dones):
+        advantages = np.zeros_like(rewards)
+        gae = 0
         for t in reversed(range(len(rewards))):
-            if t != len(rewards) - 1:
-                bootstrap_value = values[t+1]
-            else:
-                bootstrap_value = 0
-                
-            delta = rewards[t] + self.gamma * bootstrap_value * (1-dones[t]) - values[t]
-            running_advantage = delta + self.gamma * self.lambda_ * running_advantage * (1 - dones[t])
-            advantages[t] = running_advantage
-            returns[t] = running_return
-        
-        return advantages, returns
-        
-EPISODES = 1000
-DATA_COLLECTION_TIMESTEP = 1000
-
-env = gym.make('LunarLander-v2',
-               continuous=False)
-obs_space = env.observation_space.n
-agent = Agent()
-
-for i in range(EPISODES):
-    state = env.reset()
+            delta = rewards[t] + self.gamma * next_values[t] * (1 - dones[t]) - values[t]
+            gae = delta + self.gamma * self.lambda_ * (1 - dones[t]) * gae
+            advantages[t] = gae
+        return advantages
     
-    # Collect trajectories
+    def train(self, states, actions, rewards, next_states, dones):
+        states = np.array(states, dtype=np.float32)
+        actions = np.array(actions, dtype=np.int32)
+        next_states = np.array(next_states, dtype=np.float32)
+        rewards = np.array(rewards, dtype=np.float32)
+        dones = np.array(dones, dtype=np.float32)
+
+        values = tf.squeeze(self.critic(states))
+        next_values = tf.squeeze(self.critic(next_states))
+        advantages = self.compute_gae(rewards, values.numpy(), next_values.numpy(), dones)
+        returns = advantages + values.numpy()
+
+        with tf.GradientTape() as actor_tape, tf.GradientTape() as critic_tape:
+            old_probs = self.actor(states)
+            old_probs = tf.reduce_sum(old_probs * tf.one_hot(actions, self.action_dim), axis=1)
+
+            # Actor Loss
+            new_probs = self.actor(states)
+            new_probs = tf.reduce_sum(new_probs * tf.one_hot(actions, self.action_dim), axis=1)
+            ratio = new_probs / (old_probs + 1e-10)
+            surr1 = ratio * advantages
+            surr2 = tf.clip_by_value(ratio, 1.0 - self.epsilon, 1.0 + self.epsilon) * advantages
+            actor_loss = -tf.reduce_mean(tf.minimum(surr1, surr2))
+
+            # Critic Loss
+            critic_loss = 0.5 * tf.reduce_mean(tf.square(returns - tf.squeeze(self.critic(states))))
+
+            # Entropy
+            entropy = -tf.reduce_mean(self.actor(states) * tf.math.log(self.actor(states) + 1e-10))
+
+            total_loss = actor_loss + self.c1 * critic_loss - self.c2 * entropy
+
+        actor_grads = actor_tape.gradient(actor_loss, self.actor.trainable_variables)
+        critic_grads = critic_tape.gradient(critic_loss, self.critic.trainable_variables)
+        self.actor_optimizer.apply_gradients(zip(actor_grads, self.actor.trainable_variables))
+        self.critic_optimizer.apply_gradients(zip(critic_grads, self.critic.trainable_variables))
+        
+        return total_loss
+    
+EPISODES = 1000    
+SAMPLE_COLLECTION_TIMESTEP = 500
+env = gym.make('LunarLander-v2')
+agent = PPO(env.action_space.n)
+
+for episode in range(EPISODES):
+    state = env.reset()
+    state = state[0]
+    
     states = []
     rewards = []
     actions = []
+    next_states = []
     dones = []
-    old_probs = []
-    values = []
+    action_probs = []
     
-    episode_reward = 0
-    terminated = False
-    truncated = False
-    
-    for i in range(DATA_COLLECTION_TIMESTEP):
-        action, prob = agent.select_action(state)
-        
+    for i in range(SAMPLE_COLLECTION_TIMESTEP):
+        action, prob = agent.get_action(state)
         next_state, reward, terminated, truncated, _ = env.step(action)
         
-        # collect trajectories
         states.append(state)
         rewards.append(reward)
         actions.append(action)
+        action_probs.append(prob)
+        next_states.append(next_state)
         if terminated or truncated:
             dones.append(True)
         else:
             dones.append(False)
-        old_probs.append(prob)
         
         state = next_state
-
+        
         if terminated or truncated:
             state = env.reset()
-        
-    # Add value estimate for the state after the last state in the batch.
-    values = [agent.value_network(state) for state in tf.expand_dims(states, 0)]
-    next_value = agent.value_network(np.array([state]))
-    values.append(next_value)
+            state = state[0]
+            
+    loss = agent.train(states, actions, rewards, next_states, dones)
+    print('Episode: {}, Loss: {}'.format(episode, loss))
     
-    compute_advantages(states, rewards, values, dones)
+    
+env.close()
+        
+        
